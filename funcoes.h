6609ad9fc3a771d2faa0136ea91fdba9ec3c2dd5
Libanori <69210151+LibanoriJS@users.noreply.github.com>
2023-03-22 18:19:17 +1000

    Create funcoes.h

---
+++ funcoes.h
@@ -0,0 +1,17 @@
+typedef struct {
+  int idade;
+  float nota[2];
+  float frequencia;
+}Estudante;
+
+
+void recebeDados(Estudante v[], int tam);
+void apresentaMenu(void);
+float mediaIdade(Estudante v[], int tam);
+float mediaFrequencia(Estudante v[], int tam);
+int quantidadeReprovadosFreq(Estudante v[], int tam);
+int quantidadeAprovados(Estudante v[], int tam);
+float mediaDaTurma(Estudante v[], int tam);
+float maiorMediaFinal(Estudante v[], int tam);
+float menorMediaFinal(Estudante v[], int tam);
+float calculaMediaFinal(Estudante v);
